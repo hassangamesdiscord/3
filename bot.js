3d7b581f7820a5958c33a24c67127f3a55106acf
hassangamesdiscord <44375561+hassangamesdiscord@users.noreply.github.com>
2018-10-22 23:46:11 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+
+
+
+
+
+// CODES HERE
+
+
+
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
